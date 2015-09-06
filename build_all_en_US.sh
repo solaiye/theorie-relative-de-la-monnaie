ccaaaa139ff0bd65ca06833070e2a549d8836835
Vincent Texier <vit@free.fr>
2015-09-06 14:36:35 +0200

    bash command to build in en_US language
    ignore mo files

---
+++ build_all_en_US.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# GENERATE MO FILES
+sphinx-intl build --locale-dir source/locale
+
+# HTML
+# use tag for only directives (adapt image sizes for output)
+make html SPHINXOPTS="-t html -D language='en_US'"
+
+# EPUB
+make epub SPHINXOPTS="-t epub -D language='en_US'"
+cp build/epub/TheorieRelativedelaMonnaie.epub build/html/.
+
+# PDF
+make latexpdf SPHINXOPTS="-t latex -D language='en_US'"
+cp build/latex/TheorieRelativedelaMonnaie.pdf build/html/.
+
