0a8c10fa1824c76dc017ab645dd26bec6467d65e
Vincent Texier <vit@free.fr>
2017-10-28 12:31:26 +0200

    Version 1.3.7
    Add build version in english documents
    Build version is taken from VERSION file

--- build_en_US.sh
+++ build_en_US.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# set build release version in ENV to propagate it in documents
+export BUILD_RELEASE=`cat ./VERSION`
+
 # required to avoid pdf make to fail if a french latex build was made before
 rm -r build/latex
 
@@ -8,16 +11,16 @@ sphinx-intl build --locale-dir source/locale
 
 # HTML
 # use tag for only directives (adapt image sizes for output)
-make html SPHINXOPTS="-t html -D language='en_US' -D html_theme_path='themes/en_US' -D html_static_path='themes/en_US/html/static'"
+make html SPHINXOPTS="-t html -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}' -D language='en_US' -D html_theme_path='themes/en_US' -D html_static_path='themes/en_US/html/static'"
 
 # EPUB
-make epub SPHINXOPTS="-t epub -D language='en_US' -D html_theme_path='themes/en_US' -D html_static_path='themes/en_US/epub/static'"
+make epub SPHINXOPTS="-t epub -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}' -D language='en_US' -D html_theme_path='themes/en_US' -D html_static_path='themes/en_US/epub/static' -D epub_title='Relative Theory of Money v2.718 - rev. ${BUILD_RELEASE}'"
 cp build/epub/TheorieRelativedelaMonnaie.epub build/html/.
 
 # PDF
 # make latexpdf SPHINXOPTS="-t latex -D language='en_US' -D latex_elements.title='Relative Theory of Money' -D latex_elements.release='' -D latex_elements.releasename=''"
- make latexpdf SPHINXOPTS="-t latex -D language='en_US' -D latex_elements.title='Relative Theory of Money' -D latex_elements.release='2.718'"
- cp build/latex/TheorieRelativedelaMonnaie.pdf build/html/.
+make latexpdf SPHINXOPTS="-t latex -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}' -D language='en_US' -D latex_elements.title='Relative Theory of Money v2.718'"
+cp build/latex/TheorieRelativedelaMonnaie.pdf build/html/.
 
 # label folder as english version
 rm -rf build/en_US/html
--- build_fr_FR.sh
+++ build_fr_FR.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# set build release version in ENV to propagate it in documents
+export BUILD_RELEASE=`cat ./VERSION`
+
 # HTML
 # use tag for only directives (adapt image sizes for output)
-make html SPHINXOPTS="-t html"
+make html SPHINXOPTS="-t html -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}'"
 
 # EPUB
-make epub SPHINXOPTS="-t epub"
+make epub SPHINXOPTS="-t epub -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}' -D epub_title='Théorie Relative de la Monnaie v2.718 - rev. ${BUILD_RELEASE}'"
 cp build/epub/TheorieRelativedelaMonnaie.epub build/html/.
 
 # PDF
-make latexpdf SPHINXOPTS="-t latex"
+make latexpdf SPHINXOPTS="-t latex -D version='${BUILD_RELEASE}' -D release='${BUILD_RELEASE}'"
 cp build/latex/TheorieRelativedelaMonnaie.pdf build/html/.
 
 # label folder as french version
